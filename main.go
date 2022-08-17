43b1f338f6cc91b2ed6772cdecb1481fcdc37a06
Dmitry <buzindmitry.dev@gmail.com>
2022-08-17 20:27:51 +0300

    init commit

---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/joho/godotenv"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+func main() {
+	botToken := goDotEnvVariable("TELEGRAM_BOT_TOKEN")
+	botApi := "https://api.telegram.org/bot"
+	botUrl := botApi + botToken
+	offset := 0
+	for {
+		updates, err := getUpdates(botUrl, offset)
+		if err != nil {
+			log.Println("Smth went wrong", err.Error())
+		}
+		for _, update := range updates {
+			err = respond(botUrl, update)
+			offset = update.UpdateId + 1
+		}
+		fmt.Print(updates)
+	}
+}
+
+func goDotEnvVariable(key string) string {
+	err := godotenv.Load("env-variables.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
+
+func getUpdates(botUrl string, offset int) ([]Update, error) {
+	resp, err := http.Get(botUrl + "/getUpdates" + "?offset=" + strconv.Itoa(offset))
+	if err != nil {
+		return nil, err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var restResponse RestResponse
+	err = json.Unmarshal(body, &restResponse)
+	if err != nil {
+		return nil, err
+	}
+	return restResponse.Result, nil
+}
+
+func respond(botUrl string, update Update) (error error) {
+	var botMessage BotMessage
+	botMessage.ChatId = update.Message.Chat.ChatId
+	botMessage.Text = update.Message.Text
+	buf, err := json.Marshal(botMessage)
+	if err != nil {
+		return err
+	}
+
+	_, err = http.Post(botUrl+"/sendMessage", "application/json", bytes.NewBuffer(buf))
+	if err != nil {
+		return err
+	}
+	return nil
+}
